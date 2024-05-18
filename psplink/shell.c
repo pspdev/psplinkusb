3262b74f467dc6851ceb1e5e3490ee26c873efd2
Wally4000 <catapultit@me.com>
2024-05-18 19:31:31 +1000

    Remove atoi function in shell.c to use builtin

--- psplink/shell.c
+++ psplink/shell.c
@@ -1997,23 +1997,6 @@ static int remap_cmd(int argc, char **argv, unsigned int *vRet)
 	return CMD_OK;
 }
 
-// Iterative function to implement `atoi()` function in C
-static long atoi(const char* S)
-{
-    long num = 0;
- 
-    int i = 0;
- 
-    // run till the end of the string is reached, or the
-    // current character is non-numeric
-    while (S[i] && (S[i] >= '0' && S[i] <= '9'))
-    {
-        num = num * 10 + (S[i] - '0');
-        i++;
-    }
- 
-    return num;
-}
 static int meminfo_cmd(int argc, char **argv, unsigned int *vRet)
 {
 	int i;
